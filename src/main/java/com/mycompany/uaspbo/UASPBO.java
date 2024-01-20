fed0e08d73b59cb60d0de795eea468553128b743
Dysaania <ajiimansan@gmail.com>
2024-01-20 16:26:08 +0700

    first commit

---
+++ src/main/java/com/mycompany/uaspbo/UASPBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uaspbo;
+
+/**
+ *
+ * @author A S U S
+ */
+public class UASPBO {
+
+    public static void main(String[] args) {
+       new FUASPBO().setVisible(true);
+    }
+}
